4957bef7b1c441ac766202d9ae5e2d5c3048d2f0
atharvpatil15 <atharva1592005@gmail.com>
2026-04-11 15:42:59 +0530

    Repair cloudinary install files

--- scripts/postinstall-fixes.js
+++ scripts/postinstall-fixes.js
@@ -4,8 +4,21 @@ const fs = require("fs");
 const path = require("path");
 
 const rootDir = path.resolve(__dirname, "..");
-const helperDir = path.join(rootDir, "node_modules", "iconv-lite", "lib", "helpers");
-const helperFile = path.join(helperDir, "merge-exports.js");
+const iconvHelperDir = path.join(rootDir, "node_modules", "iconv-lite", "lib", "helpers");
+const iconvHelperFile = path.join(iconvHelperDir, "merge-exports.js");
+const cloudinarySdkAnalyticsDir = path.join(
+  rootDir,
+  "node_modules",
+  "cloudinary",
+  "lib",
+  "utils",
+  "encoding",
+  "sdkAnalytics"
+);
+const cloudinarySdkVersionsFile = path.join(
+  cloudinarySdkAnalyticsDir,
+  "getSDKVersions.js"
+);
 
 const mergeExportsSource = `"use strict"
 
@@ -30,12 +43,58 @@ function ensureIconvLiteHelper() {
     return;
   }
 
-  fs.mkdirSync(helperDir, { recursive: true });
+  fs.mkdirSync(iconvHelperDir, { recursive: true });
 
-  if (!fs.existsSync(helperFile)) {
-    fs.writeFileSync(helperFile, mergeExportsSource, "utf8");
+  if (!fs.existsSync(iconvHelperFile)) {
+    fs.writeFileSync(iconvHelperFile, mergeExportsSource, "utf8");
     console.log("[postinstall] Restored iconv-lite helper: lib/helpers/merge-exports.js");
   }
 }
 
+const cloudinarySdkVersionsSource = `let fs = require('fs');
+let path = require('path');
+let sdkCode = 'M'; // Constant per SDK
+
+/**
+ * @description Gets the relevant versions of the SDK(package version, node version and sdkCode)
+ * @param {'default' | 'x.y.z' | 'x.y' | string} useSDKVersion Default uses package.json version
+ * @param {'default' | 'x.y.z' | 'x.y' | string} useNodeVersion Default uses process.versions.node
+ * @return {{sdkSemver:string, techVersion:string, sdkCode:string}} A map of relevant versions and codes
+ */
+function getSDKVersions(useSDKVersion = 'default', useNodeVersion = 'default') {
+  let pkgJSONFile = fs.readFileSync(path.join(__dirname, '../../../../package.json'), 'utf-8');
+
+  // allow to pass a custom SDKVersion
+  let sdkSemver = useSDKVersion === 'default' ? JSON.parse(pkgJSONFile).version : useSDKVersion;
+
+  // allow to pass a custom techVersion (Node version)
+  let techVersion = useNodeVersion === 'default' ? process.versions.node : useNodeVersion;
+
+  return {
+    sdkSemver,
+    techVersion,
+    sdkCode
+  };
+}
+
+module.exports = getSDKVersions;
+`;
+
+function ensureCloudinarySdkAnalyticsHelper() {
+  const cloudinaryRoot = path.join(rootDir, "node_modules", "cloudinary");
+
+  if (!fs.existsSync(cloudinaryRoot)) {
+    console.warn("[postinstall] cloudinary is not installed, skipping repair.");
+    return;
+  }
+
+  fs.mkdirSync(cloudinarySdkAnalyticsDir, { recursive: true });
+
+  if (!fs.existsSync(cloudinarySdkVersionsFile)) {
+    fs.writeFileSync(cloudinarySdkVersionsFile, cloudinarySdkVersionsSource, "utf8");
+    console.log("[postinstall] Restored cloudinary helper: lib/utils/encoding/sdkAnalytics/getSDKVersions.js");
+  }
+}
+
 ensureIconvLiteHelper();
+ensureCloudinarySdkAnalyticsHelper();
